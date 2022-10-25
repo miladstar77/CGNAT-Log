f247fa74697e20d898463f1b4286ac4d5c78ce4e
Milad <51044297+miladstar77@users.noreply.github.com>
2022-10-25 20:10:07 +0330

    Update radiusDB.sh

--- radiusDB.sh
+++ radiusDB.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -u
 TIME=$(date -d "3 hour ago" +"%Y-%m-%d %H:%M:%S")
-clickhouse-client -q "select concat(database, '.', table)                         as table,  formatReadableSize(sum(bytes))                       as size,sum(rows)                                            as rows,max(modification_time)                               as latest_modification, sum(bytes)                                           as bytes_size,any(engine)                                          as engine, formatReadableSize(sum(primary_key_bytes_in_memory)) as primary_keys_size from system.parts where active group by database, table order by bytes_size desc;" -f PrettyCompact > log.txt
+clickhouse-client -q "select concat(database, '.', table) as table, formatReadableSize(sum(bytes)) as size,sum(rows) as rows,max(modification_time) as latest_modification, sum(bytes) as bytes_size,any(engine) as engine, formatReadableSize(sum(primary_key_bytes_in_memory)) as primary_keys_size from system.parts where active group by database, table order by bytes_size desc;" -f PrettyCompact > log.txt
 
 clickhouse-client  -d radius -q "alter table onlineuser  delete where (time < '$TIME')"
-
 sleep 120
-clickhouse-client -q "select concat(database, '.', table)                         as table,  formatReadableSize(sum(bytes))                       as size,sum(rows)                                            as rows,max(modification_time)                               as latest_modification, sum(bytes)                                           as bytes_size,any(engine)                                          as engine, formatReadableSize(sum(primary_key_bytes_in_memory)) as primary_keys_size from system.parts where active group by database, table order by bytes_size desc;" -f PrettyCompact >> log.txt
+clickhouse-client -q "select concat(database, '.', table) as table,  formatReadableSize(sum(bytes)) as size,sum(rows) as rows,max(modification_time) as latest_modification, sum(bytes) as bytes_size,any(engine) as engine, formatReadableSize(sum(primary_key_bytes_in_memory)) as primary_keys_size from system.parts where active group by database, table order by bytes_size desc;" -f PrettyCompact >> log.txt
